110e0b293d1ea3d598fd87eb99b12293396b3802
Stiven Benjumea <stevenbenjumea9@gmail.com>
2023-07-19 15:48:12 -0500

    register functional

--- server/src/config.js
+++ server/src/config.js
@@ -2,9 +2,9 @@ import { config } from 'dotenv'
 
 config()
 
-export const PORT = process.env.PORT || 3000
-export const DB_HOST  = process.env.DB_HOST || 'siscomite'
-export const DB_PORT = process.env.DB_PORT || 3306
-export const DB_USER = process.env.DB_USER || root
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Stigmata14/'
-export const DB_DATABASE = process.env.DB_DATABASE || 'siscomite'
+export const PORT = process.env.PORT ?? 3000
+export const DB_HOST  = process.env.DB_HOST ?? 'localhost'
+export const DB_PORT = process.env.DB_PORT ?? 3306
+export const DB_USER = process.env.DB_USER ?? 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD ?? 'Stigmata14/'
+export const DB_DATABASE = process.env.DB_DATABASE ?? 'siscomite'
