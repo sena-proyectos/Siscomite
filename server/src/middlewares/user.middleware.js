1c0cda20a6d9b77fdb2b9ef252e83b159f4a9fa3
STBenji <stevenbenjumea9@gmail.com>
2023-07-21 11:27:49 -0500

    register functional and validated fields

--- server/src/middlewares/user.middleware.js
+++ server/src/middlewares/user.middleware.js
@@ -1,5 +1,7 @@
 import { pool } from '../db.js'
+import { registerDataSchema } from '../schemas/user.schemas.js'
 import bcrypt from 'bcryptjs'
+import Joi from 'joi'
 
 export const checkUserExist = async (req, res, next) => {
     const { correo_institucional, num_documento } = req.body
@@ -17,12 +19,41 @@ export const checkUserExist = async (req, res, next) => {
     }
 }
 
+export const checkRegisterData = (req, res, next) => {
+    const { nombre, apellido, correo_institucional, correo_personal, num_telefono, num_fijo, tipo_documento, num_documento, contrasena } = req.body
+
+    const idNumberParsed = Number(num_documento)
+
+    try {
+        if (isNaN(idNumberParsed)) res.status(400).send({ message: 'El número de documento no es un número válido.' })
+        const { error } = registerDataSchema.validate({ nombre, apellido, correo_institucional, correo_personal, num_telefono, num_fijo, tipo_documento, num_documento, contrasena })
+        if (error !== undefined) return res.send({ message: 'Los datos ingresados no son válidos, verifícalos.' })
+
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error inesperado' })
+    }
+}
+
 export const hashPassword = async (req, res, next) => {
     const { contrasena } = req.body
 
     try {
         const passwordHash = await bcrypt.hash(contrasena, 10)
         req.body.contrasena = passwordHash
+        next()
+    } catch (error) {
+        res.status(500).send({ message: 'Error inesperado' })
+    }
+}
+
+export const comparePassword = async (req, res, next) => {
+    const { contrasena } = req.body
+
+    try {
+        const passwordCompare = bcrypt.compare(contrasena)
+        req.body.contrasena = passwordCompare
         next()
     } catch (error) {
         res.status(500).send({ message: 'Error inesperado' })
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { checkUserExist, hashPassword } from '../middlewares/user.middleware.js'
+import { checkUserExist, hashPassword, checkRegisterData } from '../middlewares/user.middleware.js'
 import { regiserUser, getUser } from '../controller/user.controller.js'
 
 const router = Router()
 
 router.get('/users', getUser)
-router.post('/register', checkUserExist, hashPassword, regiserUser)
+router.post('/register', checkRegisterData, checkUserExist, hashPassword, regiserUser)
 
 export default router
