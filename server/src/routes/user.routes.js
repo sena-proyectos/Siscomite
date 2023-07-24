09eb0601dedf02d38eaf3ff45eb18e1bb4bc79d4
STBenji <stevenbenjumea9@gmail.com>
2023-07-24 15:19:55 -0500

    https request added, endpoint get teachers

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { checkUserExist, hashPassword, checkRegisterData, comparePassword, checkLoginData, createToken } from '../middlewares/user.middleware.js'
-import { regiserUser, getUser, loginUser } from '../controller/user.controller.js'
+import { regiserUser, getUser, loginUser, getTeacher } from '../controller/user.controller.js'
 
 const router = Router()
 
 router.get('/users', getUser)
+router.get('/teachers', getTeacher)
+
+
 router.post('/register', checkRegisterData, checkUserExist, hashPassword, regiserUser)
 router.post('/login', checkLoginData, comparePassword, createToken, loginUser)
 
