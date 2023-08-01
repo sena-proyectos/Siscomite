30c3cc4701a4e1565f96d1fc0ebb39de6331e201
Stiven Benjumea <stevenbenjumea9@gmail.com>
2023-08-01 13:34:26 -0500

    use ref

--- client/src/Components/User/User.jsx
+++ client/src/Components/User/User.jsx
@@ -2,18 +2,24 @@ import "./User.css";
 import { Link } from "react-router-dom";
 import image from "../../assets/image/imageLogin.png";
 
+import { useRef } from "react";
+
 const User = () => {
+
+  const correoRef = useRef()
+  const contrasenaRef = useRef()
+
   return (
     <section className="container">
       <div className="main">
         <form className="loginForm">
         <h2 className="title">Iniciar Sesión</h2>
           <div className="inp">
-            <input type="text" name="document"/>
+            <input type="text" name="num_document"/>
             <label htmlFor="document">Número de documento</label>
           </div>
           <div className="inp">
-            <input type="password" name="password"/>
+            <input type="password" name="contrasena"/>
             <label htmlFor="">Contraseña</label>
           </div>
           <p>
