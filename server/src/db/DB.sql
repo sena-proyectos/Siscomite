ea0a5941805cafe044842c9032e5611c10035a46
Bedoya <2004cristiantorres@gmail.com>
2023-08-02 15:26:57 -0500

    Update Database

--- server/src/db/DB.sql
+++ server/src/db/DB.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `siscomite` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `siscomite`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: siscomite
@@ -18,6 +16,170 @@ USE `siscomite`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `aprendices`
+--
+
+DROP TABLE IF EXISTS `aprendices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aprendices` (
+  `id_aprendiz` int NOT NULL AUTO_INCREMENT,
+  `nombres_aprendiz` varchar(100) NOT NULL,
+  `apellidos_aprendiz` varchar(100) DEFAULT NULL,
+  `email_aprendiz_sena` varchar(300) NOT NULL,
+  `email_aprendiz_personal` varchar(300) NOT NULL,
+  `celular_aprendiz` varchar(20) DEFAULT NULL,
+  `fijo_aprendiz` varchar(20) DEFAULT NULL,
+  `id_documento` int NOT NULL,
+  `id_ficha` int NOT NULL,
+  PRIMARY KEY (`id_aprendiz`),
+  KEY `id_documento` (`id_documento`),
+  KEY `id_ficha` (`id_ficha`),
+  CONSTRAINT `aprendices_ibfk_1` FOREIGN KEY (`id_documento`) REFERENCES `documentos` (`id_documento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `aprendices_ibfk_2` FOREIGN KEY (`id_ficha`) REFERENCES `fichas` (`id_ficha`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aprendices`
+--
+
+LOCK TABLES `aprendices` WRITE;
+/*!40000 ALTER TABLE `aprendices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aprendices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `articulos`
+--
+
+DROP TABLE IF EXISTS `articulos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `articulos` (
+  `id_articulo` int NOT NULL AUTO_INCREMENT,
+  `numero_articulo` varchar(10) NOT NULL,
+  `prohibicion_articulo` varchar(10) DEFAULT NULL,
+  `descripcion_articulo` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_articulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articulos`
+--
+
+LOCK TABLES `articulos` WRITE;
+/*!40000 ALTER TABLE `articulos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articulos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `causas`
+--
+
+DROP TABLE IF EXISTS `causas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `causas` (
+  `id_causa` int NOT NULL AUTO_INCREMENT,
+  `categoria_causa` varchar(100) DEFAULT NULL,
+  `calificacion_causa` varchar(100) DEFAULT NULL,
+  `descripcion_caso` varchar(1000) DEFAULT NULL,
+  `evidencias` varchar(1000) NOT NULL,
+  `id_articulo` int NOT NULL,
+  PRIMARY KEY (`id_causa`),
+  KEY `id_articulo` (`id_articulo`),
+  CONSTRAINT `causas_ibfk_1` FOREIGN KEY (`id_articulo`) REFERENCES `articulos` (`id_articulo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `causas`
+--
+
+LOCK TABLES `causas` WRITE;
+/*!40000 ALTER TABLE `causas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `causas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `documentos`
+--
+
+DROP TABLE IF EXISTS `documentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `documentos` (
+  `id_documento` int NOT NULL AUTO_INCREMENT,
+  `tipo_documento` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_documento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `documentos`
+--
+
+LOCK TABLES `documentos` WRITE;
+/*!40000 ALTER TABLE `documentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `documentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fichas`
+--
+
+DROP TABLE IF EXISTS `fichas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fichas` (
+  `id_ficha` int NOT NULL AUTO_INCREMENT,
+  `numero_ficha` varchar(100) NOT NULL,
+  `programa` varchar(100) DEFAULT NULL,
+  `jornada` varchar(100) DEFAULT NULL,
+  `etapa` varchar(100) DEFAULT NULL,
+  `trimestre` varchar(100) DEFAULT NULL,
+  `id_modalidad` int NOT NULL,
+  PRIMARY KEY (`id_ficha`),
+  KEY `id_modalidad` (`id_modalidad`),
+  CONSTRAINT `fichas_ibfk_1` FOREIGN KEY (`id_modalidad`) REFERENCES `modalidades` (`id_modalidad`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fichas`
+--
+
+LOCK TABLES `fichas` WRITE;
+/*!40000 ALTER TABLE `fichas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fichas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modalidades`
+--
+
+DROP TABLE IF EXISTS `modalidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modalidades` (
+  `id_modalidad` int NOT NULL AUTO_INCREMENT,
+  `nombre_modalidad` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_modalidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modalidades`
+--
+
+LOCK TABLES `modalidades` WRITE;
+/*!40000 ALTER TABLE `modalidades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modalidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -25,10 +187,10 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `id_roles` int NOT NULL AUTO_INCREMENT,
-  `nombre_rol` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_roles`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +199,43 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Administrador'),(2,'Instructor');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitud`
+--
+
+DROP TABLE IF EXISTS `solicitud`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitud` (
+  `id_solicitud` int NOT NULL AUTO_INCREMENT,
+  `tipo_solicitud` varchar(100) DEFAULT NULL,
+  `nombre_coordinacion` varchar(200) NOT NULL,
+  `id_causa` int NOT NULL,
+  `id_usuario_solicitante` int NOT NULL,
+  `id_usuario_receptor` int NOT NULL,
+  `id_aprendiz` int NOT NULL,
+  PRIMARY KEY (`id_solicitud`),
+  KEY `fk_solicitud_1` (`id_usuario_solicitante`),
+  KEY `fk_solicitud_2` (`id_usuario_receptor`),
+  KEY `id_causa` (`id_causa`),
+  KEY `id_aprendiz` (`id_aprendiz`),
+  CONSTRAINT `fk_solicitud_1` FOREIGN KEY (`id_usuario_solicitante`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `fk_solicitud_2` FOREIGN KEY (`id_usuario_receptor`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `solicitud_ibfk_1` FOREIGN KEY (`id_causa`) REFERENCES `causas` (`id_causa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `solicitud_ibfk_2` FOREIGN KEY (`id_aprendiz`) REFERENCES `aprendices` (`id_aprendiz`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitud`
+--
+
+LOCK TABLES `solicitud` WRITE;
+/*!40000 ALTER TABLE `solicitud` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitud` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,20 +246,22 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id_usuarios` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `correo_institucional` varchar(100) NOT NULL,
-  `correo_personal` varchar(100) DEFAULT NULL,
-  `num_telefono` varchar(15) NOT NULL,
-  `num_fijo` varchar(15) DEFAULT NULL,
-  `tipo_documento` varchar(20) NOT NULL,
-  `num_documento` varchar(20) NOT NULL,
-  `contrasena` varchar(100) NOT NULL,
-  `id_roles` int NOT NULL,
-  PRIMARY KEY (`id_usuarios`),
-  KEY `fk_roles` (`id_roles`),
-  CONSTRAINT `fk_roles` FOREIGN KEY (`id_roles`) REFERENCES `roles` (`id_roles`) ON DELETE CASCADE ON UPDATE CASCADE
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `numero_documento_usuario` varchar(100) NOT NULL,
+  `email_sena` varchar(300) NOT NULL,
+  `email_personal` varchar(300) NOT NULL,
+  `celular` varchar(20) NOT NULL,
+  `teléfono_fijo` varchar(20) NOT NULL,
+  `password_usuario` varchar(500) NOT NULL,
+  `id_documento` int NOT NULL,
+  `id_rol` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_rol` (`id_rol`),
+  KEY `id_documento` (`id_documento`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`id_documento`) REFERENCES `documentos` (`id_documento`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -84,4 +283,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-19 14:14:17
+-- Dump completed on 2023-08-02 15:26:02
