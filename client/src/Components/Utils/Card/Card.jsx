3926b16e022930c09722f871e67233f8cf533b20
STBenji <stevenbenjumea9@gmail.com>
2023-08-10 11:12:03 -0500

    cards homes fixed

--- client/src/Components/Utils/Card/Card.jsx
+++ client/src/Components/Utils/Card/Card.jsx
@@ -17,7 +17,7 @@ export const Card = ({ title, descripciónHome, titleHome, descripción, image, 
 
           <section className="cardHome">
             <section className="cardImage">
-              <img className="fondoCard" src={image} alt="Imágen" />
+              <img className="fondoCard" src={image} alt="Imágen" lazyloading/>
               <div className="textoImagen">
                 <h4>{titleHome}</h4>
                 <p className="bodyText">{descripciónHome}</p>
