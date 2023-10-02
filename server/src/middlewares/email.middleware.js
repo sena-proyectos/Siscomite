931d8d71f5f8e49cd7422874be338abc074f868a
STBenji <stevenbenjumea9@gmail.com>
2023-10-02 17:17:49 -0500

    add endpoint to send emails with files

--- server/src/middlewares/email.middleware.js
+++ server/src/middlewares/email.middleware.js
@@ -4,7 +4,7 @@ export const checkEmail = (req, res, next) => {
   const { to, subject } = req.body
   try {
     const { error } = emailSchema.validate({ to, title: subject })
-    if (error !== undefined) throw new Error('Debes llenar todos los campos')
+    if (error !== undefined) return res.status(500).send({ message: 'Debes llenar todos los campos' })
     next()
   } catch (error) {
     res.status(500).send(error.message)
