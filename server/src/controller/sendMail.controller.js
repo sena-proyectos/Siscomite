34dc04e6d745622e366839606758d0e0bb3c82be
STBenji <stevenbenjumea9@gmail.com>
2023-10-03 17:39:35 -0500

    add default email template for notifications

--- server/src/controller/sendMail.controller.js
+++ server/src/controller/sendMail.controller.js
@@ -1,15 +1,15 @@
 import { emailConfig } from '../config.js'
 
 export const sendEmail = async (req, res) => {
-  const { to, subject, text } = req.body
+  const { to, subject } = req.body
   try {
     await emailConfig.sendMail({
       from: process.env.EMAIL_USERNAME,
       to,
       subject,
-      text
+      html: `<html><head><style>body{font-family:Arial,sans-serif;}.container{max-width:600px;margin:0 auto;padding:20px;}.header{background-color:#3498db;color:#fff; border-radius: 1rem ;text-align:center;padding:20px;}.content{padding:20px;}.footer{background-color:#f2f2f2;text-align:center;padding:10px;}</style></head><body><div class="container"><div class="header"><h1>¡Cordial saludo!</h1><p>Novedad en las solicitudes a comité.</p></div><div class="content"><p>Tienes un mensaje nuevo en tu bandeja de notificaciones de Siscomité. Te recomendamos revisarlo para mantenerte informado sobre las últimas novedades en las solicitudes.</p></div><div class="footer"><p>Gracias por leer este correo electrónico.</p></div></div></body></html>`
     })
-    return res.status(200).json({ message: 'Email enviado correctamente' })
+    return res.status(200).json({ message: 'Correo enviado correctamente' })
   } catch (error) {
     console.log(error)
     return res.status(500).send('Hubo un error al enviar el email')
@@ -39,6 +39,6 @@ export const sendEmailWithAttachment = async (req, res) => {
     return res.status(200).json({ message: 'Correo enviado correctamente' })
   } catch (error) {
     console.error(error)
-    return res.status(500).send({message : 'Hubo un error al enviar el correo'})
+    return res.status(500).send({ message: 'Hubo un error al enviar el correo' })
   }
 }
