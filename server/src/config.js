c4d2f9d92758da09a1bbca9b685099262c5d1fee
--global <--global>
2023-10-12 08:28:47 -0500

    MinIO added and works

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,7 @@
 // Importar el módulo de configuración de variables de entorno
 import { config } from 'dotenv'
 import nodemailer from 'nodemailer'
+import * as Minio from 'minio'
 
 // Cargar las variables de entorno desde un archivo .env
 config()
@@ -14,6 +15,8 @@ export const DB_PASSWORD = process.env.DB_PASSWORD ?? 'root' // Contraseña de l
 export const DB_DATABASE = process.env.DB_DATABASE ?? 'siscomite' // Nombre de la base de datos
 export const EMAIL_USERNAME = process.env.EMAIL_USERNAME ?? '' // Correo electrónico de la cuenta que envía los emails
 export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD ?? '' // Contraseña del correo electrónico
+export const MINIO_ROOT_USER = process.env.MINIO_ROOT_USER ?? '' // Usuario MinIO
+export const MINIO_ROOT_PASSWORD = process.env.MINIO_ROOT_PASSWORD ?? '' // Contraseña MinIO
 
 export const emailConfig = nodemailer.createTransport({
   host: 'smtp.gmail.com',
@@ -23,4 +26,12 @@ export const emailConfig = nodemailer.createTransport({
     user: EMAIL_USERNAME,
     pass: EMAIL_PASSWORD
   }
+})
+
+export const minioClient = new Minio.Client({
+  endPoint: 'localhost',
+  port: 9000,
+  accessKey: MINIO_ROOT_USER,
+  secretKey: MINIO_ROOT_PASSWORD,
+  useSSL: false
 })
