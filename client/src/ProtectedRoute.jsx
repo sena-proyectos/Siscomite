70f54435117c05c242996abde12f02f1b36867eb
STBenji <stevenbenjumea9@gmail.com>
2023-10-13 13:39:10 -0500

    protected software path

---
+++ client/src/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import Cookie from 'js-cookie'
+
+export const ProtectedRoute = ({ redirectPath = '/', allowedRoles = [] }) => {
+  const cookie = Cookie.get('token')
+
+  // Simula obtener los roles del usuario desde donde sea que los almacenes (por ejemplo, desde la cookie o el estado global).
+  const userRoles = ['Administrador', 'Instructor', 'Coordinador'] // Aquí debes obtener los roles reales del usuario
+
+  // Verifica si al menos uno de los roles del usuario coincide con los roles permitidos.
+  const hasPermission = userRoles.some((role) => allowedRoles.includes(role))
+  
+  if (!cookie || !hasPermission) {
+    return <Navigate to={redirectPath} replace />
+  }
+
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
