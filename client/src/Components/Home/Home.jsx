9c5615d3b1eabb7e28201f1685c2509e0611796a
STBenji <stevenbenjumea9@gmail.com>
2023-11-03 13:51:54 -0500

    error fixed, home, and filter groups

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -11,6 +11,27 @@ import Cookie from 'js-cookie' // Importar el módulo Cookie para trabajar con c
 import jwt from 'jwt-decode' // Importar el módulo jwt-decode para decodificar tokens JWT
 
 const Home = () => {
+  const getElementsByRole = () => {
+    const token = Cookie.get('token') // Obtener el token almacenado en las cookies
+    const information = jwt(token) // Decodificar el token JWT
+    let rolToken = information.id_rol
+
+    // Mapear los ID de rol a nombres de rol
+    if (rolToken === 1) rolToken = 'Coordinador'
+    if (rolToken === 2) rolToken = 'Instructor'
+    if (rolToken === 3) rolToken = 'Administrador'
+
+    return {
+      adminCoordi: rolToken === 'Administrador' || rolToken === 'Coordinador',
+      administration: rolToken === 'Administrador',
+      coordination: rolToken === 'Coordinador',
+      instructor: rolToken === 'Instructor'
+    }
+  }
+
+  // Obtener los elementos que se deben mostrar según el rol
+  const elements = getElementsByRole()
+
   /* matriz para las cards de acceso rapido del home */
   const data = [
     {
@@ -40,27 +61,6 @@ const Home = () => {
       Link: '/rules'
     }
   ]
-
-  const getElementsByRole = () => {
-    const token = Cookie.get('token') // Obtener el token almacenado en las cookies
-    const information = jwt(token) // Decodificar el token JWT
-    let rolToken = information.id_rol
-
-    // Mapear los ID de rol a nombres de rol
-    if (rolToken === 1) rolToken = 'Coordinador'
-    if (rolToken === 2) rolToken = 'Instructor'
-    if (rolToken === 3) rolToken = 'Administrador'
-
-    return {
-      adminCoordi: rolToken === 'Administrador' || rolToken === 'Coordinador',
-      administration: rolToken === 'Administrador',
-      coordination: rolToken === 'Coordinador',
-      instructor: rolToken === 'Instructor'
-    }
-  }
-
-  // Obtener los elementos que se deben mostrar según el rol
-  const elements = getElementsByRole()
 
   return (
     <>
