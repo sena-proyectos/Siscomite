f098bb89e9c32fd563801e4b0fc6251718479bb4
STBenji <stevenbenjumea9@gmail.com>
2023-11-03 12:43:20 -0500

    create physical folder per trainee

--- server/src/middlewares/files.middlewares.js
+++ server/src/middlewares/files.middlewares.js
@@ -2,16 +2,35 @@ import multer from 'multer'
 import fs from 'fs'
 
 // Directorio donde se guardarán los archivos subidos
-const uploadDirectory = 'uploads'
+const uploadBaseDirectory = 'C:/Archivos_SiscomiteSF'
 
 // Verificar si el directorio existe y crearlo si no
-if (!fs.existsSync(uploadDirectory)) {
-  fs.mkdirSync(uploadDirectory, { recursive: true })
+if (!fs.existsSync(uploadBaseDirectory)) {
+  fs.mkdirSync(uploadBaseDirectory, { recursive: true })
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDirectory) // Usar el directorio configurado
+    const { dataApprentice } = req.body
+
+    const dataApprenticeArray = Array.isArray(dataApprentice) ? dataApprentice : [dataApprentice]
+
+    dataApprenticeArray.forEach((data) => {
+      const uploadDirectory = `${uploadBaseDirectory}/${data}`
+
+      if (!fs.existsSync(uploadDirectory)) {
+        fs.mkdirSync(uploadDirectory, { recursive: true })
+      }
+
+      cb(null, uploadDirectory) // Usar el directorio configurado para cada archivo
+    })
+
+    const uploadDownloadDirectory = `${uploadBaseDirectory}/Docs`
+
+    if (!fs.existsSync(uploadDownloadDirectory)) {
+      fs.mkdirSync(uploadDownloadDirectory, { recursive: true })
+    }
+    cb(null, uploadDownloadDirectory)
   },
   filename: (req, file, cb) => {
     // Obtener el tipo de archivo a partir de la extensión del nombre
